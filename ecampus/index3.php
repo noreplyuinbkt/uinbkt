820daec0acc4731b347377e9b07155e96c8cab17
noreplyuinbkt <noreply@uinbukittinggi.ac.id>
2025-11-26 09:57:33 +0700

    s
    mantap

---
+++ ecampus/index3.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ecampus UIN Sjech M. Djamil Djambek Bukittinggi</title>
+	<!-- Site favicon -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700" rel="stylesheet">
+	<!-- Icon Font -->
+	<link rel="stylesheet" href="fonts/ionicons/css/ionicons.css">
+	<!-- Text Font -->
+	<link rel="stylesheet" href="fonts/font.css">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Normal style CSS -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Normal media CSS -->
+	<link rel="stylesheet" href="css/media.css">
+</head>
+<body>
+
+	
+	<!-- Header end -->
+	<main class="cd-main">
+		<section class="cd-section index3 visible">
+			<div class="cd-content style3">
+				<div class="login-box">
+					<div class="login-form-slider">
+						<!-- login slide start -->
+						<div class="login-slide slide">
+							<div class="d-flex height-100-percentage">
+								<div class="align-self-center width-100-percentage">
+									<form class="floating-form" autocomplete="off">
+										<div class="form-group">
+											<input type="text" class="form-control">
+											<label class="label">User name</label>
+										</div>
+										<div class="form-group">
+											<input type="password" class="form-control">
+											<label class="label">Password</label>
+										</div>
+										<div class="form-group">
+											<input type="password" class="form-control">
+											<label class="label">Password</label>
+										</div>
+										<div class="form-group">
+											<input type="submit" class="submit" value="Sign In">
+										</div>
+									</form>
+									<div class="forgot-txt">
+										<a href="javascript:;" class="forgot-password-click">Lupa Password</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- login slide end -->
+						<!-- forgot password slide start -->
+						<div class="forgot-password-slide slide">
+							<div class="d-flex height-100-percentage">
+								<div class="align-self-center width-100-percentage">
+									<form class="floating-form">
+										<label class="label">Masukkan Email yang terdaftar di Ecampus</label>
+										<div class="form-group user-name-field">
+											<label class="label">Email</label>
+											<input type="text" class="form-control">
+										</div>
+										<div class="form-group">
+											<input type="submit" class="submit" value="Submit">
+										</div>
+									</form>
+									<div class="sign-up-txt">
+										if you remember your password? <a href="javascript:;" class="login-click">login</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- forgot password slide end -->
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+
+	<div id="cd-loading-bar" data-scale="1" class="index"></div>
+	<!-- JS File -->
+	<script src="js/modernizr.js"></script>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/popper.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script src="js/velocity.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
